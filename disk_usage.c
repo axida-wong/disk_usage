2ff05d2d68fb818eb12f735e58ac60b69af08fbc
axida <axida15866@gmail.com>
2026-05-07 11:49:56 +0800

    继续完善

--- disk_usage.c
+++ disk_usage.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <ftw.h>
+
+typedef fileentry 
+{
+	const char * path;
+	size_t size;
+} FileEntry;
+
+FileEntry *files;
+
+void init_files()
+{
+	files = (FileEntry *)calloc(10, sizeof(FileEntry ));
+	if(files == NULL)
+	{
+		perror("Files array could not be allocated");
+		exit(-1);
+	}
+}
+
 int fn(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
-	printf("fn is called\n");
+	size_t filesize = sb->st_size;
+	// printf("%zd\t%s\n", filesize, path + ftwbuf->base);
+	printf("%zd\t%s\n", filesize, path);
 	
 	return 0;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	if(argc < 2)
+	{
+		fprintf(stderr, "Usage: %s [path]\n", argv[0]);
+		return -1;
+	}
 	printf("Yout disk usage is: \n");
 
-	nftw("./a", fn, 10, FTW_PHYS);	
+	nftw(argv[1], fn, 10, FTW_PHYS);	
 	return 0;
 }
