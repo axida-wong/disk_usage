93affc1c92b719f44d0eb0d1cec6de86a094fa41
axida <axida15866@gmail.com>
2026-05-07 19:57:59 +0800

    基本完成，差最后的美化工作

--- disk_usage.c
+++ disk_usage.c
@@ -1,29 +1,88 @@
 #include <stdio.h>
 #include <ftw.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-typedef fileentry 
+#define PATH_MAX_SIZE 4096
+#define FILES_ARR_START_SIZE 10
+
+typedef struct fileentry 
 {
-	const char * path;
+	char path[PATH_MAX_SIZE];
 	size_t size;
 } FileEntry;
 
 FileEntry *files;
 
-void init_files()
+size_t current_numb = 0;
+size_t total_numb = 0;
+
+void init_files(void)
 {
-	files = (FileEntry *)calloc(10, sizeof(FileEntry ));
+	files = (FileEntry *)calloc(FILES_ARR_START_SIZE, sizeof(FileEntry ));
+	//printf("init sucess\n");
 	if(files == NULL)
 	{
 		perror("Files array could not be allocated");
 		exit(-1);
 	}
+
+	total_numb = FILES_ARR_START_SIZE;
+}
+
+size_t max_files(FileEntry *files)
+{
+	size_t max_size = 0;
+	for(size_t i = 0; i < current_numb; i++)
+		if(max_size < files[i].size)
+			max_size = files[i].size; 
+	
+	return max_size;
+}
+	
+void print_files(void)
+{
+	size_t max = max_files(files);
+	printf("max size is %zd.\n", max);
+
+	for(size_t i = 0; i < current_numb; i++)
+	{
+		int blocks_num = (int )5 * log10((float )files[i].size / \
+			log10((float )max));
+
+		//printf("%d\t", blocks_num);
+		for(size_t j = 0; j < blocks_num; j++)
+			printf("\xe2\x96\x93");
+		printf(" %zd\t%s\n", files[i].size, files[i].path);
+	}
+}
+
+int comp_files(const void *a, const void *b) 
+{
+	FileEntry *aa, *bb;
+	aa = (FileEntry *)a;
+	bb = (FileEntry *)b;
+	//printf("%zd %zd\n", aa->size, bb->size);
+	return -(aa->size - bb->size);
 }
 
 int fn(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
-	size_t filesize = sb->st_size;
-	// printf("%zd\t%s\n", filesize, path + ftwbuf->base);
-	printf("%zd\t%s\n", filesize, path);
+	if(current_numb >= total_numb)
+	{
+		total_numb *= 2;
+		files = realloc(files, sizeof(FileEntry) * total_numb);
+		//printf("the array has been realloced\n");
+		if(files == NULL)
+		{
+			perror("Error reallocating files arr,");
+			exit(-1);
+		}
+	}
+	strcpy(files[current_numb].path, path);
+	files[current_numb].size = sb->st_size;
+	current_numb++;
 	
 	return 0;
 }
@@ -35,8 +94,13 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s [path]\n", argv[0]);
 		return -1;
 	}
-	printf("Yout disk usage is: \n");
-
+	init_files();
 	nftw(argv[1], fn, 10, FTW_PHYS);	
+	printf("Yout disk usage is: \n");
+	//print_files();
+	//printf("\n\n\n");
+	qsort(files, current_numb, sizeof(FileEntry ), comp_files);
+	print_files();
+
 	return 0;
 }
--- makefile
+++ makefile
@@ -3,7 +3,7 @@ ALL: disk_usage
 FLAGS = -Wall --std=c18 -D_XOPEN_SOURCE=700 -g
 
 disk_usage: disk_usage.c
-	gcc $(FLAGS) disk_usage.c -o disk_usage
+	gcc $(FLAGS) disk_usage.c -o disk_usage -lm
 
 clean:
 	rm disk_usage
